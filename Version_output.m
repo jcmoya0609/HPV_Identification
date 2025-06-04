e5df2bdf82801ad2ffdf2a1635f7f9ea2f0749dd
creuzige <creuzige@nist.gov>
2025-06-04 11:49:50 -0400

    Version output as a function

--- Version_output.m
+++ Version_output.m
@@ -1,9 +1,15 @@
+function Version_output(filename)
+
+%% Record the matlab, mtex versions and the date
+% Save them to a file
 
 Matlab_version=version
 Mtex_version=getMTEXpref().version
 time_now = datetime('now','TimeZone','local','Format','dd-MMM-y HH:mm:ss')
 
-fid = fopen('Version_Flag.txt','wt');
+fid = fopen(filename,'wt');
 fprintf(fid, 'Matlab version: %s\nMtex version: %s\nDate: %s',...
     Matlab_version, Mtex_version, time_now);
 fclose(fid);
+
+end
