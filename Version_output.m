6790797d6cef79a5d144e478b7219518453ff129
creuzige <creuzige@nist.gov>
2025-06-04 11:51:56 -0400

    Version output not shown on the screen

--- Version_output.m
+++ Version_output.m
@@ -3,9 +3,9 @@ function Version_output(filename)
 %% Record the matlab, mtex versions and the date
 % Save them to a file
 
-Matlab_version=version
-Mtex_version=getMTEXpref().version
-time_now = datetime('now','TimeZone','local','Format','dd-MMM-y HH:mm:ss')
+Matlab_version=version;
+Mtex_version=getMTEXpref().version;
+time_now = datetime('now','TimeZone','local','Format','dd-MMM-y HH:mm:ss');
 
 fid = fopen(filename,'wt');
 fprintf(fid, 'Matlab version: %s\nMtex version: %s\nDate: %s',...
